3f35ab1f60994a759e9f6bd811ad9cbe8a794116
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-04-12 14:41:26 +0400

    create countdown part

--- home/script.js
+++ home/script.js
@@ -6,11 +6,29 @@ startBtn.addEventListener("click", function() {
     startBtn.style.display = 'none';
     infoBox.style.display = 'block';
   });
-
-cont.addEventListener('click',function(){
-  var info_box = document.querySelector(".info_box");
-  var quiz_box = document.querySelector('.quiz_box');
-  info_box.style.display = 'none';
-  quiz_box.style.display = 'block';
+  var countdownElement = document.getElementById('countdown');
+  var countdownInterval;
   
-})
+  cont.addEventListener('click', function() {
+      var info_box = document.querySelector(".info_box");
+      var quiz_box = document.querySelector('.quiz_box');
+      info_box.style.display = 'none';
+      quiz_box.style.display = 'block';
+      var startTime = 90 * 60;
+      if (countdownInterval) {
+        clearInterval(countdownInterval);
+      }
+      countdownInterval = setInterval(function() {
+        var minutes = Math.floor(startTime / 60);
+        var seconds = startTime % 60;
+        var secondsFormatted = seconds < 10 ? '0' + seconds : seconds;
+        countdownElement.textContent = minutes + ':' + secondsFormatted;
+        startTime--;
+  
+        if (startTime < 0) {
+          clearInterval(countdownInterval);
+          countdownElement.textContent = 'Finish';
+        }
+      }, 1000);
+  });
+  

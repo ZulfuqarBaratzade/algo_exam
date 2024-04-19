a4f0c61f0d4ca2713e8a1ac831c38a8d086e5fdd
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-04-19 18:20:03 +0400

    ok

--- exam/urls.py
+++ exam/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path,include
-from .views import exam,register,user_login
+from .views import exam
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',exam,name="exam"),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('login/', user_login, name='login'),
+    path('',exam,name="exam")
+    ]
 
-]
 
 
--- exam/views.py
+++ exam/views.py
@@ -8,42 +8,6 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
-@login_required
+# @login_required
 def exam(request):
-    return render(request,"home.html",{})
-
-
-# def authView(request):
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST or None)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("login")
-#         else:
-#             form = UserCreationForm()
-#     return render(request,"login.html",{"form":form})
-from .forms import UserRegistrationForm
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('exam')  # Replace 'home' with the name of your homepage URL
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')  # Redirect to the homepage after login
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request,"home.html")
